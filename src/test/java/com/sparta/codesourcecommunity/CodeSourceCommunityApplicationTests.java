06e9a2f8d2dcccc755bc889024889d57170fb0ac
jiwon317 <chlghqlt12@naver.com>
2024-02-07 11:13:54 +0900

    first commit

---
+++ src/test/java/com/sparta/codesourcecommunity/CodeSourceCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.codesourcecommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeSourceCommunityApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
