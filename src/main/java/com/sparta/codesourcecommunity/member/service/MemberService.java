7176382746c5dec473da633537d9ba5c1780ca34
JooJuneHo <jw059@naver.com>
2024-02-08 12:29:24 +0900

    :sparkles: 로그인 기능 구현

--- src/main/java/com/sparta/codesourcecommunity/member/service/MemberService.java
+++ src/main/java/com/sparta/codesourcecommunity/member/service/MemberService.java
@@ -20,12 +20,24 @@ public class MemberService {
         String password = passwordEncoder.encode(memberRequestDto.getPassword());
         String introduce = memberRequestDto.getIntroduce();
 
-
-        if(memberRepository.findByEmail(email).isPresent()){
+        if (memberRepository.findByEmail(email).isPresent()) {
             throw new IllegalArgumentException("이미 존재하는 유저 입니다.");
         }
 
         Member member = new Member(email, nickname, password, introduce);
         memberRepository.save(member);
+    }
+
+
+    public void login(MemberRequestDto memberRequestDto) {
+        String email = memberRequestDto.getEmail();
+        String password = memberRequestDto.getPassword();
+
+        Member member = memberRepository.findByEmail(email)
+            .orElseThrow(() -> new IllegalArgumentException("등록된 유저가 없습니다."));
+
+        if (!passwordEncoder.matches(password, member.getPassword())) {
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
     }
 }
