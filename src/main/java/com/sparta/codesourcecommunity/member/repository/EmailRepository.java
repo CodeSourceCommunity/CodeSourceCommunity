27598b8874c55842b8c84c35403842a0309cfcc7
JooJuneHo <jw059@naver.com>
2024-02-12 21:28:05 +0900

    :sparkles: 회원가입시 email 인증 기능 추가(회원가입 완료 전 임시저장 Entity 하나가 필요함)

---
+++ src/main/java/com/sparta/codesourcecommunity/member/repository/EmailRepository.java
@@ -0,0 +1,11 @@
+package com.sparta.codesourcecommunity.member.repository;
+
+import com.sparta.codesourcecommunity.member.entity.Email;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmailRepository extends JpaRepository<Email, Long> {
+
+    Email findByEmail(String email);
+
+    void deleteByEmail(String email);
+}
