eedcb7d83a91f27a6a074a47d51190835e5672a6
SerenityZenDev <tjrqo2000@gmail.com>
2024-02-12 11:09:26 +0900

    :tada: 프론트 베이스 생성

--- src/main/java/com/sparta/codesourcecommunity/member/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/codesourcecommunity/member/config/WebSecurityConfig.java
@@ -47,8 +47,8 @@ public class WebSecurityConfig {
             authorizeHttpRequests
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
                 .permitAll() // resources 접근 허용 설정
-                .requestMatchers("/members/**").permitAll() // '/member/'로 시작하는 요청 모두 접근 허가
-                .anyRequest().authenticated() // 그 외 모든 요청 인증처리
+                .requestMatchers("**").permitAll() // '/member/'로 시작하는 요청 모두 접근 허가
+                //.anyRequest().authenticated() // 그 외 모든 요청 인증처리
         );
 
         // 필터 관리
