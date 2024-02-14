cd9efb480176d8e301e20587966e64ca58d1a678
SerenityZenDev <tjrqo2000@gmail.com>
2024-02-14 14:41:01 +0900

    :recycle: Board 수정에 따른 프론트 코드 변경

--- src/main/java/com/sparta/codesourcecommunity/board/dto/BoardResponseDto.java
+++ src/main/java/com/sparta/codesourcecommunity/board/dto/BoardResponseDto.java
@@ -6,11 +6,13 @@ import lombok.Getter;
 @Getter
 
 public class BoardResponseDto {
+    private Long boardId;
     private String title;
     private String contents;
     private String subtitle;
 
     public BoardResponseDto(Board board) {
+        this.boardId = board.getBoardId();
         this.title = board.getTitle();
         this.contents = board.getContents();
         this.subtitle = board.getSubtitle();
