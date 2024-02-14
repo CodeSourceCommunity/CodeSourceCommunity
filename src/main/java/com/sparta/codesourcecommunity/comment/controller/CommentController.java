5672f357e7204d310cc2d56919ee80a404fa3e42
SerenityZenDev <tjrqo2000@gmail.com>
2024-02-14 10:07:41 +0900

    :sparkles: 댓글 반환값 수정

--- src/main/java/com/sparta/codesourcecommunity/comment/controller/CommentController.java
+++ src/main/java/com/sparta/codesourcecommunity/comment/controller/CommentController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/board/{boardId}/comment")
+@RequestMapping("/boards/{boardId}/comments")
 @RequiredArgsConstructor
 public class CommentController {
 
--- src/main/java/com/sparta/codesourcecommunity/comment/dto/CommentResponseDto.java
+++ src/main/java/com/sparta/codesourcecommunity/comment/dto/CommentResponseDto.java
@@ -8,9 +8,11 @@ public class CommentResponseDto {
 
     private Long commentId;
     private String comment;
+    private String nickname;
 
-    public CommentResponseDto(Comment comment) {
+    public CommentResponseDto(Comment comment, String nickname) {
         this.commentId = comment.getCommentId();
         this.comment = comment.getContents();
+        this.nickname = nickname;
     }
 }
